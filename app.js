6e54aab1c542d58484e9ea7bdea086ca10557db7
Madina <madina.m.9509@gmail.com>
2025-02-14 13:58:11 +0500

    refactor(basic-1.1-1.2-2.1-2.2-3.1-3.2,extended-1.1-1.2-2.1):rules are used

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 //Document is the DOM can be accessed in the console with document.window.
 // Tree is from the top, html, body, p etc.
 
-//Problem: User interaction does not provide the correct results.
-//Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
-
 document.addEventListener('DOMContentLoaded', () => {
   const taskInput = document.getElementById('new-task');
   const addButton = document.getElementById('add-task');
